e3c4fd2431b53ef19a68d9c3e4b92e015cd4c7c7
aweliego <amelie.gorry.am@gmail.com>
2022-08-30 12:26:33 +0200

    Get previous timeframe text dynamically

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -2,6 +2,19 @@ import React from 'react';
 import './Activity.css';
 
 const Activity = ({ title, timeframes, timeframe }) => {
+  const getPreviousTimeframe = (timeframe) => {
+    switch (timeframe) {
+      case 'daily':
+        return 'Yesterday';
+      case 'weekly':
+        return 'Last Week';
+      case 'monthly':
+        return 'Last Month';
+      default:
+        return 'Last Week';
+    }
+  };
+
   return (
     <article className="Activity-Card">
       <div className="background-icon"></div>
@@ -9,7 +22,9 @@ const Activity = ({ title, timeframes, timeframe }) => {
         <h4 className="title">{title}</h4>
         <img src="../../../images/icon-ellipsis.svg" alt="ellipsis" />
         <p className="current-stats">{timeframes[timeframe].current}hrs</p>
-        <p className="previous-stats">Last Week - {timeframes[timeframe].previous}hrs</p>
+        <p className="previous-stats">
+          {getPreviousTimeframe(timeframe)} - {timeframes[timeframe].previous}hrs
+        </p>
       </section>
     </article>
   );
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,7 +3,6 @@ import './Dashboard.css';
 import Activity from '../Activity/Activity';
 
 const Dashboard = ({ activities, timeframe }) => {
-  //console.log(timeframe);
   return (
     <section className="Dashboard-grid">
       {activities.map((activity, idx) => (
