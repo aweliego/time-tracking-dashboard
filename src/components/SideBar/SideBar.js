74af4ee91e42a7ea68d5c8e18c0b09b2970ee8b9
aweliego <amelie.gorry.am@gmail.com>
2022-08-30 12:46:25 +0200

    Highlight active timeframe

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './SideBar.css';
+import { useState } from 'react';
 
 const SideBar = ({ timeframes, selectTimeframe }) => {
+  const [value, setValue] = useState(1);
+
   return (
     <header className="SideBar">
       <section className="user">
@@ -15,7 +18,14 @@ const SideBar = ({ timeframes, selectTimeframe }) => {
       <nav>
         <ul className="timeframes">
           {timeframes.map((timeframe, index) => (
-            <li key={index} onClick={() => selectTimeframe(timeframe)}>
+            <li
+              key={index}
+              className={index === value && 'active'}
+              onClick={() => {
+                selectTimeframe(timeframe);
+                setValue(index);
+              }}
+            >
               {timeframe}
             </li>
           ))}
