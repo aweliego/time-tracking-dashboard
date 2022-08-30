83a5902b1143f58892c02ba8683b780ca1c532a0
aweliego <amelie.gorry.am@gmail.com>
2022-08-30 14:33:43 +0200

    Generate correct background image/colour dynamically

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -2,6 +2,25 @@ import React from 'react';
 import './Activity.css';
 
 const Activity = ({ title, timeframes, timeframe }) => {
+  const getBackground = (title) => {
+    switch (title) {
+      case 'Work':
+        return 'work';
+      case 'Play':
+        return 'play';
+      case 'Study':
+        return 'study';
+      case 'Exercise':
+        return 'exercise';
+      case 'Social':
+        return 'social';
+      case 'Self Care':
+        return 'self-care';
+      default:
+        return '';
+    }
+  };
+
   const getPreviousTimeframe = (timeframe) => {
     switch (timeframe) {
       case 'daily':
@@ -16,14 +35,15 @@ const Activity = ({ title, timeframes, timeframe }) => {
   };
 
   return (
-    <article className="Activity-Card">
+    <article className={`Activity-Card ${getBackground(title)}`}>
       <div className="background-icon"></div>
       <section className="Activity">
         <h4 className="title">{title}</h4>
         <img src="../../../images/icon-ellipsis.svg" alt="ellipsis" />
         <p className="current-stats">{timeframes[timeframe].current}hrs</p>
         <p className="previous-stats">
-          {getPreviousTimeframe(timeframe)} - {timeframes[timeframe].previous}hrs
+          {getPreviousTimeframe(timeframe)} - {timeframes[timeframe].previous}
+          hrs
         </p>
       </section>
     </article>
